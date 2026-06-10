7acdc60f10d13cda1ece56469d857e7767b0687c
Kareemingit <kareemahady@gmail.com>
2026-06-11 00:41:17 +0300

    .

---
+++ FrameworkCore/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrameworkCore.Http;
+
+namespace FrameworkCore.Middlewares;
+public class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context , RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex){
+            context.Response.StatusCode = 500;
+            context.Response.Write("Internal Server Error");
+        }
+    }
+}
---
+++ FrameworkCore/Routing/RoutingTable.cs
@@ -0,0 +1,25 @@
+﻿using FrameworkCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FrameworkCore.Routing;
+
+public class RoutingTable
+{
+    private Dictionary<string, RequestDelegate> _staticRouts = new();
+    private List<KeyValuePair<List<string> , RequestDelegate>> _dynamicRouts = new();
+
+    RoutingTable()
+    {
+    }
+
+    public RequestDelegate MatchPattern(string pattern)
+    {
+        if(_staticRouts.TryGetValue(pattern , out RequestDelegate endpoint))
+            return endpoint;
+
+        return null;
+    }
+}
